2e6f59fffd5317bb89d5a43ea07be692254833e6
abinChao <aaa51507@gmail.com>
2022-03-01 17:08:09 +0800

    新增履歷內容

--- abin-profile/src/views/About/index.vue
+++ abin-profile/src/views/About/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="about">
-    <div class="col-xss-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 profile-text" style="position: relative;">
+    <div class="col-xss-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 profile-text" style="position: relative; margin-bottom: 30px;">
+        <h2 style="font-weight: blod;">曹宏斌 Abin</h2>
         <p id="title" style="font-weight: bold; font-size: 15pt; margin-bottom: 20px; ">網頁全端工程師，擅長使用 JavaScript、PHP、Groovy……等各種程式語言。</p>
 
         <p>現職於IIT內的系統維運人員，擅長團隊合作，主要負責PHP、Grails的系統維運與RWD網頁開發，從網頁前端開始學習相關的IT技術，html/css/jquery一直到現在RWD的設計切版，都非常的拿手，伺服器方面，使用Linux作業系統與PHP/MySQL來進行開發，藉此可更瞭解伺服器的相關應用，熟悉這些工具之後，才能在與業主提案討論時，快速的提供意見，讓我在溝通與合作的技巧更卓越，解決問題的能力，和案件時程的規劃，皆有專業的水準。</p>
         <p>可獨立作業，從與業主接洽需求、系統分析、資料庫的規劃、網頁切版，擁有八年多的設計經歷，一路從網頁前端，進步到全端網站設計師，具有豐富的實戰經驗，已經準備好要找個舞台來好好施展身手了。</p>
     </div>
 
-    <div class="col-xss-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 profile-img">
+    <!--div class="col-xss-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 profile-img">
         <div class="heand">
             <img class="img-responsive my-photo" src="@/assets/images/head_2018_06_25.png">
             <div class="name-block">
@@ -15,13 +16,13 @@
                 <p class="name-ch">曹宏斌</p>
             </div>
         </div>
-    </div>
+    </div-->
 
     <div class="clearfix"></div>
 
     <h2>技能</h2>
     <div class="row">
-      <div class="col-4">
+      <div class="col-xss-12 col-xs-12 col-sm-12 col-md-4" style="margin-top: 20px;">
         <h3>Front-End</h3>
         <hr/>
         <p>JavaScript: Vue/React/微信小程序/Backbone/jQuery</p>
@@ -31,7 +32,7 @@
         <p>App WebView</p>
       </div>
 
-      <div class="col-4">
+      <div class="col-xss-12 col-xs-12 col-sm-12 col-md-4" style="margin-top: 20px;">
         <h3>Back-End</h3>
         <hr/>
         <p>PHP: Laravel/CodeIgniter/Slim/OpenCart/Composer</p>
@@ -40,7 +41,7 @@
         <p>Python: django</p>
       </div>
 
-      <div class="col-4">
+      <div class="col-xss-12 col-xs-12 col-sm-12 col-md-4" style="margin-top: 20px;">
         <h3>System</h3>
         <hr/>
         <p>Linux: CentOS/Ubuntu</p>
@@ -49,14 +50,14 @@
         <p>Shell Script</p>
       </div>
 
-      <div class="col-4">
+      <div class="col-xss-12 col-xs-12 col-sm-12 col-md-4" style="margin-top: 20px;">
         <h3>Database</h3>
         <hr/>
         <p>MySQL: DbVisualizer/phpMyadmin</p>
         <p>MongoDB: Robo 3T</p>
       </div>
 
-      <div class="col-4">
+      <div class="col-xss-12 col-xs-12 col-sm-12 col-md-4" style="margin-top: 20px;">
         <h3>Other</h3>
         <hr/>
         <p>Version Control: Git/SVN/SourceTree</p>
--- abin-profile/src/views/Portfolio/project.vue
+++ abin-profile/src/views/Portfolio/project.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="project">
-    {{ $route.params.project }}
+    <div
+      v-for="p in list"
+      :key="p.id"
+      :ref="p.id"
+      class="row" style="margin-bottom: 1000px;">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <h1>
+          {{ p.name }}
+        </h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,13 +25,30 @@ export default {
   },
   computed: {
   },
+  data() {
+    return {
+      project: {},
+      list: [],
+      hasScrollIntoView: false
+    }
+  },
   mounted() {
+    window.pp = this
     let project = Portfolio.find(el => el.name === this.$route.params.project)
     if (!project) {
       this.$router.push({ path: '/portfolio' })
+      this.$destroy(true)
     }
+    this.project = project
+    this.list = Portfolio.filter(el => el.company === this.project.company)
   },
   methods: {
+  },
+  updated() {
+    if (this.list.length && !this.hasScrollIntoView) {
+      window.scroll(0, this.$refs[this.project.id][0].offsetTop - document.getElementsByTagName('header')[0].clientHeight - 30)
+      this.hasScrollIntoView = true
+    }
   }
 }
 </script>
